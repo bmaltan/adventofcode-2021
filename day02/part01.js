0c6154d6778d52c691f36ce086f65d18a40a7700
Bugra Murat Altan <bmaltan@live.com>
2021-12-05 11:21:27 +0100

    formatting

--- day02/part01.js
+++ day02/part01.js
@@ -1,29 +1,29 @@
 import { movements } from './test.js';
 
 function getLocDelta(movements) {
-    let depth = 0;
-    let horizontalPos = 0;
+  let depth = 0;
+  let horizontalPos = 0;
 
-    movements.forEach(movement => {
-        const vector = movement.split(' ');
-        const delta = parseInt(vector[1]);
+  movements.forEach(movement => {
+    const vector = movement.split(' ');
+    const delta = parseInt(vector[1]);
 
-        switch (vector[0]) {
-            case 'forward':
-                horizontalPos += delta;
-                break;
-            case 'down':
-                depth += delta;
-                break;
-            case 'up':
-                depth -= delta;
-                if (depth < 0) depth = 0;
-                break;
-            default:
-        }
-    })
+    switch (vector[0]) {
+      case 'forward':
+        horizontalPos += delta;
+        break;
+      case 'down':
+        depth += delta;
+        break;
+      case 'up':
+        depth -= delta;
+        if (depth < 0) depth = 0;
+        break;
+      default:
+    }
+  })
 
-    return depth * horizontalPos || horizontalPos;
+  return depth * horizontalPos || horizontalPos;
 }
 
 getLocDelta(movements);
--- day02/part02.js
+++ day02/part02.js
@@ -1,31 +1,31 @@
 import { movements } from './test.js';
 
 function getLocDelta(movements) {
-    let depth = 0;
-    let horizontalPos = 0;
-    let aim = 0;
+  let depth = 0;
+  let horizontalPos = 0;
+  let aim = 0;
 
-    movements.forEach(movement => {
-        const vector = movement.split(' ');
-        const delta = parseInt(vector[1]);
+  movements.forEach(movement => {
+    const vector = movement.split(' ');
+    const delta = parseInt(vector[1]);
 
-        switch (vector[0]) {
-            case 'forward':
-                horizontalPos += delta;
-                depth += aim * delta;
-                break;
-            case 'down':
-                aim += delta;
-                break;
-            case 'up':
-                aim -= delta;
-                if (depth < 0) depth = 0;
-                break;
-            default:
-        }
-    })
+    switch (vector[0]) {
+      case 'forward':
+        horizontalPos += delta;
+        depth += aim * delta;
+        break;
+      case 'down':
+        aim += delta;
+        break;
+      case 'up':
+        aim -= delta;
+        if (depth < 0) depth = 0;
+        break;
+      default:
+    }
+  })
 
-    return depth * horizontalPos || horizontalPos;
+  return depth * horizontalPos || horizontalPos;
 }
 
 console.log(getLocDelta(movements));
